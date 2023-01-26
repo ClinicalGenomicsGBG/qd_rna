1b25477bcf15f29fca301d73af36d42da3eb5ef0
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-01-26 11:34:33 +0100

    Refactor Nextflow exit logic

--- scripts/nextflow.sh
+++ scripts/nextflow.sh
@@ -4,6 +4,8 @@ set -e -o pipefail
 source ${_MODULES_INIT}
 module load nextflow
 HOME="$(pwd)" nextflow $@ & _nf_pid=$!
-trap "echo 'Killing nextflow...'; kill -TERM $_nf_pid; wait $_nf_pid" 2 3 6 10 12 14 15
+
+trap "kill -0 $_nf_pid && echo 'Killing nextflow...' && kill -TERM $_nf_pid; wait $_nf_pid" EXIT
+
 wait $_nf_pid
-ß
+exit $?
