103652dc78c783855c7c2950ee55dba5bab32949
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-02-21 08:39:20 +0100

    Squashed 'modules/hcp/' changes from ddbec45..6af7650
    6af7650 Update to efcf655: Various cleanup
    0526409 Update to 730ec15: Rework results handling
    a3d5ffd Update to 1f14394: Rework results handling
    fc23056 Update to 8b5a1a9: Rework results handling
    3c68db6 Update to 1ed9b33: Rework results handling
    5c186b0 Update to 79a8c2d: Rework results handling
    4e510c3 Update to 601603f: Fix parallel value from config
    7eecb8d Update to fa28d88: Use concurrent.futures ProcessPoolExecutor
    f28af14 Update to da29c61: Use concurrent.futures ProcessPoolExecutor
    060bf81 Update to ea8a4c8: Use concurrent.futures ProcessPoolExecutor
    ce7a652 Update to 671429a: Convert fastq path to str
    a5451a6 Update to 78e02fe: Rework HCP search logic
    f197e3a Update to 41e76ae: Rework HCP search logic
    c933159 Update to b03b607: Rework HCP search logic
    807c22e Update to be2f27d: Handle empty fastq_paths
    15368a7 Update to 2b92042: Fix typo
    d857083 Update to 7cf2984: Fix typo
    993b2ec Update to 1816673: Separate HCP and petausite modules
    3b38fb0 Update to 19a42a6: Per-path logging
    853d271 Update to 3f39303: Per-path logging
    d0fff39 Update to 5dca114: Update hook labels
    28a65de Update to b4457d4: Backup needs to be iterable
    
    git-subtree-dir: modules/hcp
    git-subtree-split: 6af7650f0c377aabf2caf800b1b6582521200e70

--- __init__.py
+++ __init__.py
@@ -1,75 +1,68 @@
 """Module for fetching files from HCP."""
 
-import multiprocessing as mp
-import os
+from concurrent.futures import ProcessPoolExecutor, Future
 import sys
+from functools import partial
 from logging import LoggerAdapter
-from multiprocessing.connection import Connection
 from pathlib import Path
 from typing import Optional
 
 from NGPIris import hcp
 
-from cellophane import data, cfg, modules, sge
+from cellophane import cfg, data, modules
 
 
 def _fetch(
-    local_path: Path,
     config: cfg.Config,
-    pipe: Connection,
+    local_path: Path,
     remote_key: Optional[str] = None,
 ) -> None:
-    sys.stdout = open(os.devnull, "w", encoding="utf-8")
-    sys.stderr = open(os.devnull, "w", encoding="utf-8")
+    sys.stdout = open(
+        config.logdir / f"iris.{local_path.name}.out", "w", encoding="utf-8"
+    )
+    sys.stderr = open(
+        config.logdir / f"iris.{local_path.name}.err", "w", encoding="utf-8"
+    )
 
     hcpm = hcp.HCPManager(
         credentials_path=config.iris.credentials,
         bucket="data",  # FIXME: make this configurable
     )
 
-    _local_path = local_path
-    while Path(_local_path).suffix:
-        _local_path = _local_path.with_suffix("")
+    if remote_key is None:
+        search_path = local_path
+        while Path(search_path).suffix:
+            search_path = search_path.with_suffix("")
 
-    if (_remote_key := remote_key) is None:
-        remote = hcpm.search_objects(_local_path.name)
-        if remote is not None and len(remote) == 1:
-            _remote_key = remote[0].key
+        result = hcpm.search_objects(search_path.name)
+        if result is not None and len(result) == 1:
+            remote_key = result[0].key
         else:
-            raise SystemExit(1)
-
-    if (suffix := Path(_remote_key).suffix) in (".fasterq", ".fastq", ".fastq.gz"):
-        _local_path = _local_path.with_suffix(suffix)
-    else:
-        raise SystemExit(1)
-
-    if not _local_path.exists():
+            raise ValueError(f"Could not find remote key for {local_path}")
+    if not local_path.exists():
         hcpm.download_file(
-            _remote_key,
-            local_path=str(_local_path),
+            remote_key,
+            local_path=str(local_path),
             callback=False,
             force=True,
         )
 
-    if _local_path.suffix == ".fasterq":
-        if not _local_path.with_suffix(".fastq.gz").exists():
-            sge.submit(
-                str(Path(__file__).parent / "scripts" / "petasuite.sh"),
-                f"-d -f -t {config.petasuite.sge_slots} {_local_path}",
-                env={"_MODULES_INIT": config.modules_init},
-                queue=config.petasuite.sge_queue,
-                pe=config.petasuite.sge_pe,
-                slots=config.petasuite.sge_slots,
-                name="petasuite",
-                stderr=config.logdir / "petasuite.err",
-                stdout=config.logdir / "petasuite.out",
-                cwd=local_path.parent,
-                check=True,
-            )
-        _local_path = _local_path.with_suffix(".fastq.gz")
 
-    pipe.send(_local_path)
-    pipe.close()
+def _fetch_callback(
+    future: Future,
+    /,
+    logger: LoggerAdapter,
+    samples: data.Samples,
+    local_path: Path,
+    s_idx: int,
+    f_idx: int,
+):
+    if (exception := future.exception()) is not None:
+        logger.error(f"Failed to fetch {local_path} from HCP ({exception})")
+        samples[s_idx].fastq_paths[f_idx] = None
+    else:
+        logger.debug(f"Fetched {local_path}")
+        samples[s_idx].fastq_paths[f_idx] = str(local_path)
 
 
 @modules.pre_hook(label="HCP", priority=10)
@@ -80,47 +73,41 @@ def hcp_fetch(
     **_,
 ) -> data.Samples:
     """Fetch files from HCP."""
+    with ProcessPoolExecutor(config.iris.parallel) as pool:
+        for s_idx, sample in enumerate(samples):
+            if all(
+                fastq is not None and Path(fastq).exists()
+                for fastq in sample.fastq_paths
+            ):
+                logger.debug(f"Files found for {sample.id} {sample.fastq_paths}")
 
-    _procs: list[tuple[mp.Process, int, int, Connection]] = []
+            elif "remote_keys" in sample.backup:
+                logger.info(f"Fetching files for {sample.id} from HCP")
+                if "remote_keys" not in sample.backup:
+                    logger.warning(f"Remote key not found for {sample.id}, will search")
+                    sample.backup.remote_keys = [None] * len(sample.fastq_paths)
 
-    for s_idx, sample in enumerate(samples):
-        if all(Path(p).exists() for p in sample.fastq_paths):
-            logger.debug(
-                f"Files found for {sample.id} ({','.join(sample.fastq_paths)})"
-            )
-        else:
-            logger.info(f"Fetching files for {sample.id} from HCP")
-            if "remote_keys" not in sample.backup:
-                logger.warning(f"Remote key not found for {sample.id}, will search")
-                sample.backup.remote_keys = [None, None]
-            for f_idx, fastq in enumerate(sample.fastq_paths):
-                remote_key: Optional[str] = (
-                    sample.backup.remote_keys[f_idx]
-                    if "remote_keys" in sample.backup
-                    else None
-                )
+                for f_idx, local_key in enumerate(sample.fastq_paths):
+                    remote_key: Optional[str] = sample.backup.remote_keys[f_idx]
+                    _local_key = local_key or remote_key or f"{sample.id}_{f_idx}"
+                    local_path = config.iris.fastq_temp / Path(_local_key).name
 
-                local_path = config.iris.fastq_temp / Path(fastq).name
-
-                _in, _out = mp.Pipe()
-                proc = mp.Process(
-                    target=_fetch,
-                    kwargs={
-                        "pipe": _in,
-                        "remote_key": remote_key,
-                        "local_path": local_path,
-                        "config": config,
-                    },
-                )
-                proc.start()
-                _procs.append((proc, s_idx, f_idx, _out))
-
-    for proc, s_idx, f_idx, pipe in _procs:
-        proc.join()
-        if proc.exitcode != 0:
-            logger.error(f"Failed to fetch {samples[s_idx].id}")
-            samples[s_idx].fastq_paths[f_idx] = None
-        else:
-            samples[s_idx].fastq_paths[f_idx] = pipe.recv()
+                    pool.submit(
+                        _fetch,
+                        config=config,
+                        local_path=local_path,
+                        remote_key=remote_key,
+                    ).add_done_callback(
+                        partial(
+                            _fetch_callback,
+                            logger=logger,
+                            samples=samples,
+                            local_path=local_path,
+                            s_idx=s_idx,
+                            f_idx=f_idx,
+                        )
+                    )
+            else:
+                logger.warning(f"Unable to fetch files for {sample.id} from HCP")
 
     return samples.__class__([s for s in samples if s is not None])
