db2ede29d3421948a23bdfe862c8efb375c61275
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-03-17 16:02:59 +0100

    Use mixin class

--- modules/qd_rna.py
+++ modules/qd_rna.py
@@ -24,11 +24,6 @@ class QDRNASamples(data.Mixin):
     output: list[Output] = []
 
 
-@modules.pre_hook(label="QD-RNA Mixin")
-def qd_rna_mixin(samples: data.Samples, **_):
-    samples.add_mixin(QDRNASamples)
-    return samples
-
 @modules.post_hook(label="Copy results")
 def copy_webstore(samples: QDRNASamples, logger: LoggerAdapter, config: cfg.Config, **_):
     logger.info("Copying to config.results_base")
