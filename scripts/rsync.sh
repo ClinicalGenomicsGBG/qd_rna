1fea1761644295cb29b88ff121876ef76aa0a26c
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-04-20 17:36:02 +0200

    Squashed 'modules/rsync/' changes from cd0f25a..72efaaa
    72efaaa Update to a29e5e4: Fixup rsync module
    
    git-subtree-dir: modules/rsync
    git-subtree-split: 72efaaa340d9c35a4e237d80bcfd74da6a37d0ae

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo "Copying files from ${SRC} to ${DST}"
 
-rsync -a ${SRC} ${DST}
+SRC_ARRAY=( $SRC )
+DST_ARRAY=( $DST )
+
+for sidx in ${!SRC_ARRAY[@]}; do
+    src=${SRC_ARRAY[$sidx]}
+    dst=${DST_ARRAY[$sidx]}
+    echo "Syncing files from ${src} to ${dst}"
+    rsync -a $src $dst
+done
