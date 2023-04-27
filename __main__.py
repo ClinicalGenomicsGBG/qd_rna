01e59d43082a66b2684c55995b7dc0ff42e8bc95
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-04-27 11:57:57 +0200

    Set root in __main__ to avoid slow inspect call

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from cellophane import cellophane
+from pathlib import Path
+
 
 if __name__ == "__main__":
-    _main = cellophane("QD-RNA")
+    _main = cellophane("QD-RNA", root=Path(__file__).parent)
     _main(prog_name="qd_rna")
 
