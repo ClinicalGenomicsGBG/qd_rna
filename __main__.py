10caff82fe479b7a62aba135e6ffff7a45a530e9
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-05-04 16:19:24 +0200

    Update __main__ to new structure

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
+import sys
 from cellophane import cellophane
 from pathlib import Path
 
 
 if __name__ == "__main__":
-    _main = cellophane("QD-RNA", root=Path(__file__).parent)
+    _root = Path(__file__).parent
+    sys.path.append(str(_root))
+    _main = cellophane("QD-RNA", root=_root)
     _main(prog_name="qd_rna")
-
