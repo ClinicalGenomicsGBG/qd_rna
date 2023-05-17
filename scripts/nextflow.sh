6e8b5a696c75a27dacce9b3be28f02b65b19b7a0
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-05-17 15:26:29 +0200

    Squashed 'modules/nextflow/' changes from 0b53a62..486afa9
    486afa9 Update to 0b580c2: feat: Create release module branches
    REVERT: 0b53a62 Update to 4575ea9: Various fixes with new interfaces
    REVERT: 54a27c9 Update to 8bc4093: Update modules to new mixin style
    REVERT: 62a007e Update to a8abb47: Fix workflow
    
    git-subtree-dir: modules/nextflow
    git-subtree-split: 486afa9fe1f3e7f90ffd7f5177dbb9d548d04d86

--- scripts/nextflow.sh
+++ scripts/nextflow.sh
@@ -2,6 +2,18 @@
 
 set -e -o pipefail
 
+_clean () {
+    code=$?
+    ps -p $_nxf_pid &> /dev/null && {
+        echo "Killing process..."
+        kill -TERM $_nxf_pid
+        wait $_nxf_pid
+        exit $?
+    } || exit $code
+}
+
 module load $_JAVA_MODULE
 module load $_NXF_MODULE
-NXF_HOME="${TMPDIR}/.nextflow" nextflow $@
+NXF_HOME="${TMPDIR}/.nextflow" nextflow $@ & _nxf_pid=$!
+trap _clean EXIT
+wait $_nxf_pid
