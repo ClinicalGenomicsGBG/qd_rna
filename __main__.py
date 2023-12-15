f4055f0db0a4c28f2ca7165f6421b285f5acc5a3
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-12-15 15:11:17 +0100

    feat(cellophane): Initial commit from cellophane 🎉

---
+++ __main__.py
@@ -0,0 +1,9 @@
+import sys
+from cellophane import cellophane
+from pathlib import Path
+
+if __name__ == "__main__":
+    _root = Path(__file__).parent
+    sys.path.append(str(_root))
+    _main = cellophane("qd_rna", root=_root)
+    _main(prog_name="qd_rna")
