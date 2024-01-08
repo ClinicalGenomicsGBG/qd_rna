982cf4e804b3ccf9f6547452bae304ddb1153909
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-01-08 14:25:51 +0100

    fix: Run field is in sample.meta

--- modules/qd_rna.py
+++ modules/qd_rna.py
@@ -17,7 +17,7 @@ def set_sample_id(
     known_dups: set[str] = set()
     for sample in samples:
         dups = [s for s in _samples if s.id == sample.id]
-        runs = set(sorted(d.run for d in dups if "run" in d))
+        runs = set(sorted(d.meta.run for d in dups if "run" in d.meta))
 
         if (n := len(dups)) > 1 and config.merge:
             if sample.id not in known_dups:
@@ -26,15 +26,15 @@ def set_sample_id(
             sample.id = f"{sample.id}_{sorted(runs)[-1]}" if runs else sample.id
             merge_file = workdir / f"{sample.id}.merged_runs.txt"
             merge_file.write_text("\n".join(runs))
-            sample.output += [data.Output(src=[merge_file], dest_dir=Path(sample.id))]
-        elif n > 1 and not config.merge and "run" in sample:
-            sample.id = f"{sample.id}_{sample.run}"
-        elif n > 1 and not config.merge and "run" not in sample:
+            sample.output += [data.Output(src=merge_file, dst=Path(sample.id).merged_runs.txt)]
+        elif n > 1 and not config.merge and "run" in sample.meta:
+            sample.id = f"{sample.id}_{sample.meta.run}"
+        elif n > 1 and not config.merge and "run" not in sample.meta:
             if sample.id not in known_dups:
                 logger.warning(f"Will merge {n} samples with shared id {sample.id}")
             known_dups |= {sample.id}
 
         else:
-            sample.id = f"{sample.id}_{sample.run}" if "run" in sample else sample.id
+            sample.id = f"{sample.id}_{sample.meta.run}" if "run" in sample.meta else sample.id
 
     return samples
